5d4df0d414d466cd31f18426e06139aab6ddbbe5
Michael Schatzl <michael.schatzl@at.bosch.com>
2023-12-20 16:43:08 +0100

    201223

---
+++ Lieferliste_WPF/ViewModels/MeasuringRoomViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CompositeCommands.Core;
+using El2Core.ViewModelBase;
+using GongSolutions.Wpf.DragDrop;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lieferliste_WPF.ViewModels
+{
+    internal class MeasuringRoomViewModel : ViewModelBase, IDropTarget
+    {
+        private IContainerProvider _container;
+        private IApplicationCommands _applicationCommands;
+        public MeasuringRoomViewModel(IContainerProvider container, IApplicationCommands applicationCommands)
+        { 
+            _container = container;
+            _applicationCommands = applicationCommands;
+        }
+        public void DragOver(IDropInfo dropInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
