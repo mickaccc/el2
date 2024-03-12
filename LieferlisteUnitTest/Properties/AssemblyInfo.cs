9fa06960c00531f0831fd6ee49169fed7a1fa47c
mickaccc <mg.schatzl@gmail.com>
2024-03-12 21:32:09 +0100

    120324

--- LieferlisteUnitTest/Properties/AssemblyInfo.cs
+++ LieferlisteUnitTest/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("LieferlisteUnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LieferlisteUnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("26b84ffa-2437-425e-a359-860450d0302e")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
