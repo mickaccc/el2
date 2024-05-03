4bb1c280170d30a47982f6f5d94c8f1502f897b5
mickaccc <mg.schatzl@gmail.com>
2024-05-03 21:30:37 +0200

    030524

---
+++ El2Utilities/Utils/PersonalFilter.cs
@@ -0,0 +1,54 @@
+﻿using El2Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace El2Core.Utils
+{
+    public class PersonalFilterContainer
+    {
+        Dictionary<string, PersonalFilter> _filters = [];
+        public void Add(string name, PersonalFilter filter)
+        {
+            _filters.Add(name, filter);
+        }
+    }
+    public class PersonalFilter
+    {
+        private string _RuleCommand;
+        private string _Property;
+        private PropertyNames _Prop;
+
+        public PropertyNames Prop => _Prop;
+        public string RuleCommand => _RuleCommand;
+        public string Property => _Property;
+        public PersonalFilter(string rule, string prop) { _RuleCommand = rule; _Property = prop; }
+        public bool TestValue(Vorgang vorgang)
+        {
+            Regex regex = new Regex(_RuleCommand);
+            PropertyInfo info = vorgang.GetType().GetProperty(_Property);
+            if (info != null)
+            {
+                var get = info.GetValue(vorgang, null);
+                
+                return regex.Match(get.ToString()).Success;
+            }
+            return false;
+        }
+
+
+    }
+    public struct PropertyNames
+    {
+        public static string Auftragsnummer = "Aid";
+        public static string Material = "AidNavigation.Material";
+
+
+    }
+
+}
--- Lieferliste_WPF/ViewModels/MainWindowViewModel.cs
+++ Lieferliste_WPF/ViewModels/MainWindowViewModel.cs
@@ -600,6 +600,11 @@ namespace Lieferliste_WPF.ViewModels
         }
         private void DbOperations()
         {
+            var pcont = new PersonalFilterContainer();
+            var filt = new PersonalFilter("^F", PropertyNames.Auftragsnummer);
+            pcont.Add("name", filt);
+            var res = filt.TestValue(new Vorgang() { Aid = "f2100", BemM = "V" });
+            
 
            
         }
