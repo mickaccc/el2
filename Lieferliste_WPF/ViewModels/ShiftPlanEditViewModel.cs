fc996cd5a6e5604be2a9306ab30ced286fbf55d5
mickaccc <mg.schatzl@gmail.com>
2024-06-09 23:23:53 +0200

    090624

---
+++ Lieferliste_WPF/ViewModels/ShiftPlanEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lieferliste_WPF.ViewModels
+{
+    internal class ShiftPlanEditViewModel
+    {
+        private IContainerProvider _container;
+        public string Title { get; } = "Schichtplan";
+        public ImmutableDictionary<int, bool[]> ShiftPlan { get; }
+        public ShiftPlanEditViewModel(IContainerProvider container)
+        {
+            _container = container;
+            Dictionary<int, bool[]> dict = [];
+            for(int i=0; i<7;i++)
+            {
+                dict.Add(i, new bool[1440]);
+            }
+            ShiftPlan = dict.ToImmutableDictionary();
+        }
+    }
+}
--- Lieferliste_WPF/Views/ShiftPlanEdit.xaml.cs
+++ Lieferliste_WPF/Views/ShiftPlanEdit.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lieferliste_WPF
+namespace Lieferliste_WPF.Views
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for ShiftPlanEdit.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class ShiftPlanEdit : UserControl
     {
-        public Window1()
+        public ShiftPlanEdit()
         {
             InitializeComponent();
         }
