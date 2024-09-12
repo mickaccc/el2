d73d01db376d5c2bbccf5cb5719d4bf965389b82
scm2hl <michael.schatzl@at.bosch.com>
2024-09-12 13:59:33 +0200

    120924

---
+++ ModuleShift/ShiftModule.cs
@@ -0,0 +1,18 @@
+
+
+namespace ModuleShift
+{
+    public class ShiftModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+
+}
