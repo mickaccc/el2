0430087aeb89ca19109d00d7e5ee380db884a215
mickaccc <mg.schatzl@gmail.com>
2025-03-17 18:06:38 +0100

    1703254

---
+++ WpfCustomControlLibrary/ExpandPopup.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCustomControlLibrary
+{
+    public class ExpandPopup : ItemsControl
+    {
+        static ExpandPopup()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ExpandPopup), new FrameworkPropertyMetadata(typeof(ExpandPopup)));
+        }
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsOpen.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(ExpandPopup), new PropertyMetadata(false));
+
+
+    }
+}
